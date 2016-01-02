10404660d86300a41b18774c4f912fa5d1eac41f
Pierre Gourlaouen <pi.gourlaouen@gmail.com>
2016-01-02 01:38:28 +0100

    Correction

---
+++ 301-custom-images-at-scale/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ADDRESS=`ifconfig eth0 | grep "inet addr" | sed 's/.*addr:\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]\).*/\1/'`
+HOSTNAME=`echo $(hostname)`
+REPLACEMENT='s/br-pre-apache1/'$HOSTNAME'/g'
+
+sed -i -- $REPLACEMENT /etc/zabbix/zabbix_agentd.conf
+
