03d19e17bb772dc5483d141dde91bb3bbf8a1584
Pierre Gourlaouen <pi.gourlaouen@gmail.com>
2016-01-05 18:57:11 +0100

    Correction

---
+++ 301-custom-redis-at-scale/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ADDRESS=`ifconfig eth0 | grep "inet addr" | sed 's/.*addr:\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]\).*/\1/'`
+HOSTNAME=`echo $(hostname)`
+REPLACEMENT='s/br-pre-apache1/'$HOSTNAME'/g'
+
+sudo sed -i -- $REPLACEMENT /etc/zabbix/zabbix_agentd.conf
+
+sudo service zabbix-agent restart
