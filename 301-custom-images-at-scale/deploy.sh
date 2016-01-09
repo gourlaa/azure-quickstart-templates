5b00d73a106c633b851b4bec6b6133a04bb8b06a
Pierre Gourlaouen <pi.gourlaouen@gmail.com>
2016-01-09 03:37:07 +0100

    Correction

--- 301-custom-images-at-scale/deploy.sh
+++ 301-custom-images-at-scale/deploy.sh
@@ -6,4 +6,14 @@ REPLACEMENT='s/br-pre-apache1/'$HOSTNAME'/g'
 
 sudo sed -i -- $REPLACEMENT /etc/zabbix/zabbix_agentd.conf
 
+
+sudo sed -i -- 's/-preprod.braineet.com/.braineet.com/g'  /etc/apache2/sites-available/braineet.conf
+sudo sed -i -- 's/.preprod.braineet.com/.braineet.com/g'  /etc/apache2/sites-available/braineet.conf
+sudo sed -i -- 's/preprod.braineet.com/braineet.com/g'  /etc/apache2/sites-available/braineet.conf
+
+sudo sed -i -- 's/ServerName braineet.com/ServerName braineet.com\\nServerAlias www.braineet.com/g'  /etc/apache2/sites-available/braineet.conf
+
+
+sudo service apache2 restart
+
 sudo service zabbix-agent restart
