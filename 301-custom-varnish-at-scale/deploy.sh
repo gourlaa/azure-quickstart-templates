632b44b9e91ad1ad830ef79723d49b0da23ff96d
Pierre Gourlaouen <pi.gourlaouen@gmail.com>
2016-01-09 03:10:32 +0100

    Correction

--- 301-custom-varnish-at-scale/deploy.sh
+++ 301-custom-varnish-at-scale/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ADDRESS=`ifconfig eth0 | grep "inet addr" | sed 's/.*addr:\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]\).*/\1/'`
+ADDRESS=`ifconfig eth0 | grep "inet addr" | sed 's/.*addr:\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*/\1/'`
 HOSTNAME=`echo $(hostname)`
 REPLACEMENT='s/br-pre-varnish1/'$HOSTNAME'/g'
 
@@ -8,7 +8,13 @@ sudo sed -i -- $REPLACEMENT /etc/zabbix/zabbix_agentd.conf
 
 sudo sed -i -- 's/preprod.braineet.com/braineet.com/g' /etc/nginx/sites-available/default
 
-sudo sed -i -- 's/10.0.0.7/10.0.1.6/g' /etc/varnish/default.vcl
+sudo sed -i -- 's/10.0.0.4/10.0.1.6/g' /etc/varnish/default.vcl
+
+sudo wget https://raw.githubusercontent.com/gourlaa/azure-quickstart-templates/master/301-custom-varnish-at-scale/general.crt
+sudo mv general.crt /etc/nginx/ssl/
+
+sudo sed -i -- 's/public.crt/general.crt/g' /etc/nginx/sites-available/default
+
 
 sudo service varnish restart
 sudo service nginx restart
